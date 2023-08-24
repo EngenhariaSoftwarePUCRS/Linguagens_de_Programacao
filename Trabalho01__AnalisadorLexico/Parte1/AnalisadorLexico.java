877d995fb62e5a290ac7f593f8bd5f1e18520ccd
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2023-08-24 19:53:05 -0300

    T1 - Finalizado - 24/08/23

--- Trabalho01__AnalisadorLexico/Parte1/AnalisadorLexico.java
+++ Trabalho01__AnalisadorLexico/Parte1/AnalisadorLexico.java
@@ -1,11 +1,17 @@
-package Trabalho01__AnalisadorLexico;
+package Parte1;
 
 public class AnalisadorLexico {
     public static void main(String[] args) {
         String text = args.length > 0 ? args[0] : "/* comentario teste */";
-        if (text.startsWith("/*") && text.endsWith("*/"))
+        System.out.printf("Texto analisado: %s%n", text);
+        if (isValidComment(text))
             System.out.println("Comentário válido!");
         else
             System.out.println("Comentário inválido!");
+    }
+
+    static boolean isValidComment(String text) {
+        text = text.trim();
+        return text.startsWith("/*") && text.endsWith("*/");
     }
 }
---
+++ Trabalho01__AnalisadorLexico/README.md
@@ -0,0 +1,6 @@
+Integrantes do Grupo:
+- Brenda Camara
+- Felipe Freitas
+- Luiza Heller
+
+Link youtube: [T1 - Linguagens de Programação](https://www.youtube.com/watch?v=)
